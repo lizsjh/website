09da01e4426ef35f3003cdeb02e16f17abb3e9a5
lizsjh <37032480+lizsjh@users.noreply.github.com>
2021-04-11 14:06:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -25,6 +25,22 @@ botui.message.add({
     return botui.message.add({
         delay:700,
         loading: true,
+        content:'I can help you with that. First, could you tell me why you need to replace or return this textbook?'
+    });
+}).then(function(){
+    return botui.action.text({
+        action: {
+          placeholder: 'Enter your message.'
+        }
+    
+    });
+}).then(function (res) { 
+    console.log(res.value);
+    response.push(res.value);
+}).then(function(){
+    return botui.message.add({
+        delay:700,
+        loading: true,
         content:'Got it. Could you input your order number below?'
     });
 }).then(function(){
@@ -44,13 +60,48 @@ botui.message.add({
         content:'Alright. I am checking your order right now. Please give me a moment.'
     });
 }).then(function(){
-    sendcomplete();
     return botui.message.add({
         delay:1200,
         loading: true,
-        content:'We are currently experiencing a system error. Please contact us again later.'
+        content:'I have pulled up your order. The 3rd edition is available, and you will be charged an additional $50.'
+    });
+}).then(function(){
+    return botui.message.add({
+        delay:900,
+        loading: true,
+        content:'Meanwhile, is there anything else you need?'
+    });
+}).then(function(){
+    return botui.action.text({
+        action: {
+          placeholder: 'Enter your message.'
+        }
+    
+    });
+}).then(function (res) { 
+    console.log(res.value);
+    response.push(res.value);
+}).then(function(){
+    return botui.message.add({
+        delay:700,
+        loading: true,
+        content:'Okay. Please hold on for a moment.'
+    });
+}).then(function(){
+    return botui.message.add({
+        delay:1200,
+        loading: true,
+        content:'I have processed your request. An email will be sent shortly about your new order.'
+    });
+}).then(function(){
+    sendcomplete();
+    return botui.message.add({
+        delay:700,
+        loading: true,
+        content:'The issue is resolved. Please contact us again if you need further assistance. Bye.'
     });
 });
+
 
 function sendcomplete(){
     window.parent.postMessage({"message": "completed","text":response}, "*");
